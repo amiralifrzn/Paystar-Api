aa509a0dab65ce2adaebb2440b15e6af1ea56ebb
amirali <amirali_forouzan@yahoo.com>
2021-07-31 21:18:14 +0430

    add name and phone number for login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Auth\Events\Verified;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Http\Response;
@@ -14,12 +16,14 @@ class AuthController extends Controller
         $fields = request()->validate([
             'name' => 'required|string',
             'email' => 'required|string|unique:users,email',
+            'phone' => 'required|string|max:20|unique:users',
             'password' => 'required|string|confirmed'
         ]);
 
         $user = User::create([
             'name' => $fields['name'],
             'email' => $fields['email'],
+            'phone' => $fields['phone'],
             'password' => bcrypt($fields['password']),
         ]);
 
@@ -30,6 +34,7 @@ class AuthController extends Controller
             'token' => $token
         ];
 
+        event(new Registered($user));
         return response($response, 201);
     }
 
@@ -40,16 +45,28 @@ class AuthController extends Controller
             'password' => 'required|string'
         ]);
 
-        $user = User::where('email', $fields['email'])->first();
-        if (!$user || !Hash::check($fields['password'], $user->password)){
+        $login = $fields['email'];
+
+        if(is_numeric($login)){
+            $field = 'phone';
+        } elseif (filter_var($login, FILTER_VALIDATE_EMAIL)) {
+            $field = 'email';
+        } else {
+            $field = 'name';
+        }
+
+        request()->merge([$field => $login]);
+        if(auth()->attempt(array($field => $fields['email'], 'password' => $fields['password'])))
+        {
+            $token = auth()->user()->createToken('pstoken')->plainTextToken;
+        }else{
             return response([
-                'msg' => 'wrong email or password!'
+                'msg' => 'wrong username or password!'
             ],401);
         }
-        $token = $user->createToken('pstoken')->plainTextToken;
 
         $response = [
-            'user' => $user,
+            'user' => auth()->user(),
             'token' => $token
         ];
 
@@ -63,5 +80,25 @@ class AuthController extends Controller
         return [
             'msg' => 'logged out!'
         ];
+    }
+
+    public function verify($id, $hash)
+    {
+        $user = User::find($id);
+        abort_if(!$user, 403);
+        abort_if(!hash_equals($hash, sha1($user->getEmailForVerification())), 403);
+
+        if(!$user->hasVerifiedEmail()){
+            $user->markEmailAsVerified();
+            event(new Verified($user));
+        }
+        return view('verified_account');
+    }
+
+    public function resendNotification(Request $request)
+    {
+        $request->user()->sendEmailVerificationNotification();
+
+        return back()->with('message', 'Verification link sent!');
     }
 }
