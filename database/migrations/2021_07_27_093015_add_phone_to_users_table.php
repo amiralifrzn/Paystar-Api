7ccde7496b0c815ce7821ce5c2f8c4fb8c43fd12
amirali <amirali_forouzan@yahoo.com>
2021-08-05 18:38:21 +0430

    correct down method

--- database/migrations/2021_07_27_093015_add_phone_to_users_table.php
+++ database/migrations/2021_07_27_093015_add_phone_to_users_table.php
@@ -26,7 +26,7 @@ class AddPhoneToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('phone');
+            $table->dropColumn('phone');
         });
     }
 }
